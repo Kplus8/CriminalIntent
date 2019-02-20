f88455c571c2a88a930fde71e70ca914e99af2d2
Kplus8 <35977528+Kplus8@users.noreply.github.com>
2019-02-19 19:23:17 -0700

    Starter Code

---
+++ app/src/main/java/com/csci448/labs/criminalintent/CrimeDetailsActivity.kt
@@ -0,0 +1,6 @@
+package com.csci448.labs.criminalintent
+
+// TODO implement SingleFragmentActivity
+class CrimeDetailsActivity {
+
+}
---
+++ app/src/main/java/com/csci448/labs/criminalintent/CrimeDetailsFragment.kt
@@ -0,0 +1,19 @@
+package com.csci448.labs.criminalintent
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import kotlinx.android.synthetic.main.fragment_details.*
+
+// TODO create the fragment
+class CrimeDetailsFragment : Fragment() {
+
+
+
+}
